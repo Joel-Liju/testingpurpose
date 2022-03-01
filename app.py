7a8b0b29761beb28ce5ff24fa4b71a70504984d7
Joel-Liju <69165450+Joel-Liju@users.noreply.github.com>
2022-03-01 18:36:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tp.db'
+db = SQLAlchemy(app)
+
+class Info(db.Model):
+    __tablename__='data'
+    name = db.Column(
+      db.String(8),
+      primary_key=True
+    )
+    age = db.Column(
+      db.String(8)
+    )
+    
+    def __repr__(self):
+      return self.name+" "+self.age
+    
+
+db.create_all()
 
 @app.route('/')
 def tester():
